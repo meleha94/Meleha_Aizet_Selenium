1b5f6c4c46cf4f7671b015f859acfb9a6b67d2e0
Meleha <MelehaYousaf.qa@gmail.com>
2022-03-20 16:05:20 -0400

    Initial commit 1

---
+++ src/test/java/meleha_Selenium/HelloWorld.java
@@ -0,0 +1,7 @@
+package meleha_Selenium;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Selenium part of the practice ");
+    }
+}
