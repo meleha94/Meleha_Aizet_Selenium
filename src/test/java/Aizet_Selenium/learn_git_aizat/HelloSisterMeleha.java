cd31ec6f908f176a548528f5f8cc231f7506f921
Aizat Kalybekova <aizat.kalybekova@yahoo.com>
2022-03-20 16:10:25 -0500

    new converstaion added

--- src/test/java/Aizet_Selenium/learn_git_aizat/HelloSisterMeleha.java
+++ src/test/java/Aizet_Selenium/learn_git_aizat/HelloSisterMeleha.java
@@ -4,6 +4,7 @@ public class HelloSisterMeleha {
     public static void main(String[] args) {
         System.out.println("new class added");
         System.out.println("Hi how are you? We can have a simple conversation");
+        System.out.println("I am good how are you? ");
 
     }
 }
