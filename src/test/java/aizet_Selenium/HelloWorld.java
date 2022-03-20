fb8fbb784ab30546eeb692d83506d82dde9f4f23
Meleha <81922092+meleha94@users.noreply.github.com>
2022-03-20 16:59:40 -0400

    Delete src/test/java/aizet_Selenium directory

--- src/test/java/aizet_Selenium/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package Aizet_Selenium;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Selenium part of the practice ");
-    }
-}
