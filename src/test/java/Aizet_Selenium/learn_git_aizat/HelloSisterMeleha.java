02c8b80c3a499634ae3720faa02312c691b5608f
Meleha <MelehaYousaf.qa@gmail.com>
2022-03-20 16:54:18 -0400

    onceMore

--- src/test/java/Aizet_Selenium/learn_git_aizat/HelloSisterMeleha.java
+++ src/test/java/Aizet_Selenium/learn_git_aizat/HelloSisterMeleha.java
@@ -3,6 +3,7 @@ package Aizet_Selenium.learn_git_aizat;
 public class HelloSisterMeleha {
     public static void main(String[] args) {
         System.out.println("new class added");
+        System.out.println("Hi how are you? We can have a simple conversation");
 
     }
 }
