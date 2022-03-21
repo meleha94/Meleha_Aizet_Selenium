4868d30963166f2aa38210a965694b87d4a51196
Meleha <MelehaYousaf.qa@gmail.com>
2022-03-21 16:57:30 -0400

    Java Only Practice For array

--- src/test/java/meleha_Selenium/Java/ArrayPractice/FindTheFrequenceyOfTheElement.java
+++ src/test/java/meleha_Selenium/Java/ArrayPractice/FindTheFrequenceyOfTheElement.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class FindTheFrequenceyOfTheElement {
     public static void main(String[] args) {
         //Java Program to find the frequency of each element in the array
-        System.out.println("First you would need ");
+        System.out.println("First you would need create an Array where you will be store an array,\n" +
+                "2. You would need to create another where you need to provide the size of the first array ");
         int [] arr = {1,2,8,3,2,2,2,5,1};
         int [] fr = new int [arr.length];
         int visited = -1;
