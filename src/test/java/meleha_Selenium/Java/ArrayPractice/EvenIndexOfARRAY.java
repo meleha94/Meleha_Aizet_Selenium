622cd19ccbc56e7790bc34181fa58f067bb99748
Meleha <MelehaYousaf.qa@gmail.com>
2022-03-23 17:59:17 -0400

    Printing The even position in Array

---
+++ src/test/java/meleha_Selenium/Java/ArrayPractice/EvenIndexOfARRAY.java
@@ -0,0 +1,10 @@
+package meleha_Selenium.Java.ArrayPractice;
+
+public class EvenIndexOfARRAY {
+    public static void main(String[] args) {
+        int [] again = {23,1,2321,54,2314,23,12,2,5};
+        for (int i = 1; i<again.length-2; i++,i++){
+            System.out.print(again[i]+" ");
+        }
+    }
+}
