05b54deb251e7371d3ed6f755795816ab8c23b7f
Meleha <MelehaYousaf.qa@gmail.com>
2022-03-23 01:13:45 -0400

    Printing The reverse of an Array

--- src/test/java/meleha_Selenium/Java/ArrayPractice/ReverseOrderForArray.java
+++ src/test/java/meleha_Selenium/Java/ArrayPractice/ReverseOrderForArray.java
@@ -3,7 +3,9 @@ package meleha_Selenium.Java.ArrayPractice;
 public class ReverseOrderForArray {
     public static void main(String[] args) {
         int [] arr = {6,5,7,8,4,9,12,65,78,4,8,54};
-        //int [] reverseArr = new int[arr.length];
+         // we need to start write the array, then loop through them by making the intial same as array
+        // length and then decrement it by one, LOOP THROUGH UNTIL IT I IS NOT LESS THAN 0
+
         for(int i=arr.length-1;i>=0;i-- ){
          System.out.print(arr[i]+" ");
         }
