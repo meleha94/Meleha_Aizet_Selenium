bd2bee2cac449c523cd22d53de5a0e14cc8ed856
Meleha <MelehaYousaf.qa@gmail.com>
2022-03-24 14:42:49 -0400

    update on the even positing of the array as well, asscending the order of the Array

---
+++ src/test/java/meleha_Selenium/Java/ArrayPractice/AssendingOrderOfArray.java
@@ -0,0 +1,23 @@
+package meleha_Selenium.Java.ArrayPractice;
+
+import java.util.Arrays;
+
+public class AssendingOrderOfArray {
+    public static void main(String[] args) {
+        //
+        int [] originalArray = {5,2,8,7,1};
+        int seting = 0;
+        //int [] accedningOrder = new int [originalArray.length];
+        for (int i = 0; i < originalArray.length; i++) {
+            for (int j = i+1; j < originalArray.length; j++) {
+                if(originalArray[i] > originalArray[j]) {
+                    seting = originalArray[i];
+                    originalArray[i] = originalArray[j];
+                    originalArray[j] = seting;
+                }
+            }
+        }
+        System.out.println("assending "+ Arrays.toString(originalArray));
+     //   System.out.println("Assending: "+Arrays.toString(accedningOrder));
+    }
+}
--- src/test/java/meleha_Selenium/Java/ArrayPractice/EvenIndexOfARRAY.java
+++ src/test/java/meleha_Selenium/Java/ArrayPractice/EvenIndexOfARRAY.java
@@ -2,8 +2,16 @@ package meleha_Selenium.Java.ArrayPractice;
 
 public class EvenIndexOfARRAY {
     public static void main(String[] args) {
+        /*
+        If i would need to strat with index of 1 and then increase with 2 inorder to
+        getting the even posting of an array
+         */
+
+
+        //              1  2 3    4  5    6  7  8 9
         int [] again = {23,1,2321,54,2314,23,12,2,5};
-        for (int i = 1; i<again.length-2; i++,i++){
+        //              0, 1 ,2,  3,  4   5  6  7 8
+        for (int i = 1; i<again.length; i++,i++){
             System.out.print(again[i]+" ");
         }
     }
