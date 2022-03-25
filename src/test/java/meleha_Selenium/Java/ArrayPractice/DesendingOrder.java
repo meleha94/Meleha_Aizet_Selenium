50a1b9357b9c9f704e35bb1dded52918c401c5af
Meleha <MelehaYousaf.qa@gmail.com>
2022-03-25 01:58:16 -0400

    descending order

--- src/test/java/meleha_Selenium/Java/ArrayPractice/DesendingOrder.java
+++ src/test/java/meleha_Selenium/Java/ArrayPractice/DesendingOrder.java
@@ -6,12 +6,12 @@ public class DesendingOrder {
     public static void main(String[] args) {
 
         // when decending order
-        // 1. creat one Array
-        // 2. create a vraible where it is = 0;
-        // 3. creat a for loop where i is starting with 0
-        // 4. create another for loop where the inital j is starting at i+1
-        // 5. create an if statment where index of arr i is less then index of array j
-        // 6. then place the value in the created vraiable
+        // 1. create one Array
+        // 2. create a variable where it is = 0;
+        // 3. create a for loop where i is starting with 0
+        // 4. create another for loop where the initial j is starting at i+1
+        // 5. create an if statement where index of arr i is less than index of array j
+        // 6. then place the value in the created variable
         // 7. then assign index if i to the j
         // 8. then assign index of j to stored variable
 
